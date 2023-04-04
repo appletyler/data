61e3622f158f49e7c8fbda1c0db205b32d7424dd
appletyler <apple_tyler@mail.ru>
2023-04-04 20:27:48 +0300

    Initial commit

---
+++ src/Data.java
@@ -0,0 +1,20 @@
+public class Data {
+    public static void main(String[] args) {
+        byte b = 0x55;
+        short s = 0x55ff;
+        int i = 1000000;
+        long l = 0xffffffffL;
+        char с = 'a';
+        float f = .25f;
+        double d = .00001234;
+        boolean bool = true;
+        int x = 9;
+        int y = 8;//5
+        int z=x+y;//5
+        int t = z+(x*y);//5
+        int p = 1000000;
+        int min = p/60;
+        int h = min/60;
+        System.out.println(h);
+    }
+}
